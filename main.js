e3ac2ba122ee87da7041294bea44859b4e28ab20
CleitonMtx <98102500+CleitonMtx@users.noreply.github.com>
2022-03-01 17:35:56 -0300

    Update IMC
    UPdate do imc.

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ minhafuncao = function (){
   var idade = window.prompt('Qual é a sua idade?')
   var altura = window.prompt('Qual é a sua altura')
   var peso = window.prompt('Qual é o seu peso?')
-  var imc = (peso/altura)^2
-
-  document.write(
-    `${nome}, seu IMC é: ${imc}`
-  )
+  var alturaDiv = altura * altura
+  var imc = (peso/alturaDiv)^0.5
+  var somaNo = NomeComp.length
+  var somaNoId = parseInt(somaNo) + parseInt(idade)
+  var deathD = parseInt(idade) * parseInt(365.5)
+  var deathH = parseInt(idade) * parseInt(8772)
+  var deatM = parseInt(idade) * parseInt(526320)
+  document.write(`${nome}!!! <br/>Você sabia que... <br/> Seu IMC é: ${imc}
+  <br/> Seu nome completo tem  ${somaNo} letras
+  <br/> Se somar seu nome à sua idade o resultado é: ${somaNoId} 
+  <br/> Você tem aproximadamente ${deathD} dias, ${deathH} Horas, <br/> e ${deatM} minutos de vida!`)
 }
